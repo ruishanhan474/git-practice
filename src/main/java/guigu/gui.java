d74d2721ce4d33e141e9c99b069428f7a80b0fbd
ruishan <16604125462@163.com>
2026-07-14 21:36:07 +0200

    second commit

--- src/main/java/guigu/gui.java
+++ src/main/java/guigu/gui.java
@@ -3,5 +3,6 @@ package guigu;
 public class gui {
     public static void main (String[] args){
         System.out.println("hello and have a nice day git");
+        System.out.println("hello again");
     }
 }
