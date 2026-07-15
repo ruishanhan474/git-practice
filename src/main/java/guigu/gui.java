3152fd97c2fb91478bb5877094db7106036cdee4
ruishan <16604125462@163.com>
2026-07-15 18:57:26 +0200

    hot-fix commit

--- src/main/java/guigu/gui.java
+++ src/main/java/guigu/gui.java
@@ -3,6 +3,6 @@ package guigu;
 public class gui {
     public static void main (String[] args){
         System.out.println("hello and have a nice day git");
-        System.out.println("hello again");
+        System.out.println("hello hot-fix");
     }
 }
