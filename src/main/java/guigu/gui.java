34bf1f16a2147eab51d7cde44bda0dfaddc938cd
ruishan <16604125462@163.com>
2026-07-15 20:01:24 +0200

    this is git-fix commit

--- src/main/java/guigu/gui.java
+++ src/main/java/guigu/gui.java
@@ -4,6 +4,6 @@ public class gui {
     public static void main (String[] args){
         System.out.println("hello and have a nice day git");
         System.out.println("hello master-fix");
-        System.out.println("hello this is hot-fix");
+        System.out.println("hello this is git-fix");
     }
 }
